82408ca90106add2a4d2fc4acf5dfdd89b2818c1
rang <withghd@gmail.com>
2025-04-01 16:36:11 +0900

    feat: 사용자 회원가입 시 이름, 이메일, 비밀번호 Validation 적용

--- src/main/java/task/schedule/dto/CreateUserRequest.java
+++ src/main/java/task/schedule/dto/CreateUserRequest.java
@@ -1,20 +1,27 @@
 package task.schedule.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 
 @Getter
 public class CreateUserRequest {
 
     @NotBlank(message = "이름을 입력하세요.")
+    @Size(max = 10, message = "이름은 10자를 초과할 수 없습니다.")
+    @Pattern(regexp = "^[\\p{L}]+$", message = "이름은 문자만 입력 가능합니다.")
     private final String name;
 
-    @Email(message = "올바른 이메일 형식이 아닙니다.")
     @NotBlank(message = "이메일을 입력하세요.")
+    @Email(message = "올바른 이메일이 아닙니다.")
+    @Pattern(regexp = "^[^@\\s]+@[^@\\s]+\\.[^@\\s]+$", message = "올바른 이메일이 아닙니다.")
     private final String email;
 
     @NotBlank(message = "비밀번호를 입력하세요.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$",
+            message = "비밀번호는 영문자, 숫자, 특수문자가 포함되어야 합니다."
+    )
     private final String password;
 
     public CreateUserRequest(String name, String email, String password) {
