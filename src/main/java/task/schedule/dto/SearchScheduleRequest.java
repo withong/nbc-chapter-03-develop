42a7c415d3843e0fc6696a36f4af8a5ca2cacc91
rang <withghd@gmail.com>
2025-04-03 11:29:54 +0900

    fix: final 제거

--- src/main/java/task/schedule/dto/SearchScheduleRequest.java
+++ src/main/java/task/schedule/dto/SearchScheduleRequest.java
@@ -17,18 +17,18 @@ public class SearchScheduleRequest {
 
     @Min(value = 1900, message = "1900년 이후부터 조회 가능합니다.")
     @Max(value = 2100, message = "조회 가능한 연도 범위를 초과했습니다.")
-    private final Integer year;
+    private Integer year;
 
     @Min(value = 1, message = "유효하지 않은 날짜입니다.")
     @Max(value = 12, message = "유효하지 않은 날짜입니다.")
-    private final Integer month;
+    private Integer month;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
-    private final LocalDate date;
+    private LocalDate date;
 
     @Size(max = 50, message = "제목은 50자를 초과할 수 없습니다.")
-    private final String title;
+    private String title;
 
     @Size(max = 200, message = "내용은 200자를 초과할 수 없습니다.")
-    private final String content;
+    private String content;
 }
